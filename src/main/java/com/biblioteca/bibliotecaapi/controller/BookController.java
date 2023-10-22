a15408a12d169ace26376b7d28827016d2c1e9bd
jvitoralb <jvvalb@gmail.com>
2023-10-22 00:33:20 -0300

    add base enpoints + responses

--- src/main/java/com/biblioteca/bibliotecaapi/controller/BookController.java
+++ src/main/java/com/biblioteca/bibliotecaapi/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.biblioteca.bibliotecaapi.controller;
 
+import com.biblioteca.bibliotecaapi.domain.model.Book;
 import com.biblioteca.bibliotecaapi.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
 @RestController
@@ -15,12 +18,19 @@ public class BookController {
     }
 
     @GetMapping
-    public ResponseEntity<String> getAll() {
-        return ResponseEntity.ok("All books");
+    public ResponseEntity<Iterable<Book>> getAll() {
+        return ResponseEntity.status(200).body(service.getAllBooks());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getSingle(@PathVariable String id) {
-        return ResponseEntity.ok("Books: " + id);
+    public ResponseEntity<Book> getSingle(@PathVariable UUID id) {
+        Book book = service.getBookById(id);
+        return ResponseEntity.status(200).body(book);
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> postBook(@RequestBody Book book) {
+        Book insertedBook = service.insertBook(book);
+        return ResponseEntity.status(201).body(insertedBook);
     }
 }
