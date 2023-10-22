40c3abdb0ae1ee3c60f4487befaa68164b52754f
jvitoralb <jvvalb@gmail.com>
2023-10-22 00:33:35 -0300

    fix

--- src/main/java/com/biblioteca/bibliotecaapi/domain/repository/BookRepository.java
+++ src/main/java/com/biblioteca/bibliotecaapi/domain/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package com.biblioteca.bibliotecaapi.domain.repository;
 
+import com.biblioteca.bibliotecaapi.domain.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 
 @Repository
-public class BookRepository {
-}
+public interface BookRepository extends JpaRepository<Book, UUID> { }
