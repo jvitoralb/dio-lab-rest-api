f01b59c5708a6ea6545e2afb61633425446b1579
jvitoralb <jvvalb@gmail.com>
2023-10-22 00:34:00 -0300

    add base services

--- src/main/java/com/biblioteca/bibliotecaapi/service/BookService.java
+++ src/main/java/com/biblioteca/bibliotecaapi/service/BookService.java
@@ -1,8 +1,32 @@
 package com.biblioteca.bibliotecaapi.service;
 
 
+import com.biblioteca.bibliotecaapi.domain.model.Book;
+import com.biblioteca.bibliotecaapi.domain.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class BookService {
+    @Autowired
+    BookRepository repository;
+
+    public Iterable<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    public Book getBookById(UUID id) {
+        Optional<Book> foundBook = repository.findById(id);
+        if (foundBook.isEmpty()) {
+            throw new RuntimeException("Could not find book");
+        }
+        return foundBook.get();
+    }
+
+    public Book insertBook(Book book) {
+        return repository.save(book);
+    }
 }
