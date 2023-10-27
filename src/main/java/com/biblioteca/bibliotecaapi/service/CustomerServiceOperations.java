386fa6d12072c1060ee93d3f3ac6153adda5e783
jvitoralb <jvvalb@gmail.com>
2023-10-27 12:34:02 -0300

    rm unecessary interfaces

--- src/main/java/com/biblioteca/bibliotecaapi/service/CustomerServiceOperations.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.bibliotecaapi.service;
-
-import com.biblioteca.bibliotecaapi.dao.model.Customer;
-
-import java.util.UUID;
-
-public interface CustomerServiceOperations extends ServiceOperations<Customer, UUID> {
-    Customer getOneByCPF(String cpf);
-}
