ef9f4f798212c3069853f75953c1fe54206cdbd6
jvitoralb <jvvalb@gmail.com>
2023-11-04 23:49:52 -0300

    update home

--- src/main/java/com/biblioteca/bibliotecaapi/controller/HomeController.java
+++ src/main/java/com/biblioteca/bibliotecaapi/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.biblioteca.bibliotecaapi.controller;
 
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public ResponseEntity<String> getHomePage() {
-        return ResponseEntity.status(200).body("Bem vindo ao Gestor da Biblioteca!");
+        return ResponseEntity.status(200).contentType(MediaType.TEXT_HTML).body(
+            "<h1>Bem vindo à API da Biblioteca!</h1>" +
+            "<ul>" +
+                "<li><a href=\"/swagger-ui/index.html\">Docs</a></li>" +
+                "<li><a href=\"https://github.com/jvitoralb/dio-lab-rest-api\" target=\"_blank\">Repo</a></li>" +
+            "</ul>"
+        );
     }
 }
