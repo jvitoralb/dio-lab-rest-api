aeb1e194882bce171812f44c42f36c6d97b52e57
jvitoralb <jvvalb@gmail.com>
2024-05-29 18:39:26 -0300

    fix: insert operation return value

--- src/main/java/com/biblioteca/bibliotecaapi/service/ServiceOperations.java
+++ src/main/java/com/biblioteca/bibliotecaapi/service/ServiceOperations.java
@@ -3,7 +3,7 @@ package com.biblioteca.bibliotecaapi.service;
 import java.util.List;
 
 public interface ServiceOperations<T, K> {
-    void insert(T t);
+    T insert(T t);
     T getOne(K k);
     List<T> getAll();
     T update(K k, T t);
